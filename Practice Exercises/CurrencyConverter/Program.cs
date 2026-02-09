a50567df88bf0b8a0bb093aa0584bf949b84f445
Laurence <mendozalaurence0819@gmail.com>
2026-02-09 09:31:51 +0800

    Add files via upload

---
+++ Practice Exercises/CurrencyConverter/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Runtime.ConstrainedExecution;
+
+class CurrencyConverter
+{
+    static void Main()
+    {
+        Console.Write("Enter amount in USD: ");
+        double usd = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter exchange rate from USD to EUR: ");
+        double rate = Convert.ToDouble(Console.ReadLine());
+
+        double eur = usd * rate;
+
+        Console.WriteLine("Amoune in EUR: " + eur.ToString("F2"));
+        Console.ReadLine();
+
+    }
+
+}
